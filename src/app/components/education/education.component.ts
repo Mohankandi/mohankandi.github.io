baaee10221b45737ec3cc102705de35f610edc96
Mohan <mohankandi31@gmail.com>
2025-03-02 22:47:12 -0600

    fix: update education component to match project card image handling

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Education } from 'src/app/models/education';
+import { DomSanitizer, SafeStyle } from "@angular/platform-browser";
 
 @Component({
   selector: 'education',
@@ -8,4 +9,27 @@ import { Education } from 'src/app/models/education';
 })
 export class EducationComponent {
   @Input() education: Education;
+  public backGroundImg: SafeStyle;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  getImageString(): string {
+    if (!this.education || !this.education.collegeName) {
+      return null;
+    }
+    return this.education.collegeName
+      .toLowerCase()
+      .split(" ")
+      .join("")
+      .toString()
+      .concat(".png");
+  }
+
+  ngOnChanges(changes) {
+    if (changes && changes.education) {
+      this.backGroundImg = this.sanitizer.bypassSecurityTrustStyle(
+        `url('../../../dist/assets/${this.getImageString()}')`
+      );
+    }
+  }
 }
