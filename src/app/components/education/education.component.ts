057de4406f3c7d0673722749be86a3a26d2ace52
Mohan <mohankandi31@gmail.com>
2025-03-02 22:52:57 -0600

    fix

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Education } from 'src/app/models/education';
-import { DomSanitizer, SafeStyle } from "@angular/platform-browser";
 
 @Component({
   selector: 'education',
@@ -9,27 +8,4 @@ import { DomSanitizer, SafeStyle } from "@angular/platform-browser";
 })
 export class EducationComponent {
   @Input() education: Education;
-  public backGroundImg: SafeStyle;
-
-  constructor(private sanitizer: DomSanitizer) {}
-
-  getImageString(): string {
-    if (!this.education || !this.education.collegeName) {
-      return null;
-    }
-    return this.education.collegeName
-      .toLowerCase()
-      .split(" ")
-      .join("")
-      .toString()
-      .concat(".png");
-  }
-
-  ngOnChanges(changes) {
-    if (changes && changes.education) {
-      this.backGroundImg = this.sanitizer.bypassSecurityTrustStyle(
-        `url('../../../dist/assets/${this.getImageString()}')`
-      );
-    }
-  }
 }
